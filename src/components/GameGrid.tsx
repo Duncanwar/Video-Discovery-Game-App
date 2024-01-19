4ce203439b4f2650b0319ce950ab1596d9e2e613
Duncanwar <semunda9@gmail.com>
2024-01-19 12:05:53 +0200

    Building Sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,8 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface Props{
@@ -16,6 +15,7 @@ const GameGrid = ({gameQuery}:Props) => {
 
   return (
     <>
+    {console.log(gameQuery,"here")}
       {error && <Text> {error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
