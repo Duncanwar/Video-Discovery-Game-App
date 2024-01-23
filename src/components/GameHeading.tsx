0708524640ba1f1ad2e789bbb91f3408b7a1dc24
Duncanwar <semunda9@gmail.com>
2024-01-23 15:50:55 +0200

    add a dynamic page heading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,9 +8,9 @@ interface Props{
 
 const GameHeading = ({gameQuery}:Props) => {
 
-    const heading = `${gameQuery.platform?.name} ${gameQuery.genre}`
+    const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name} Games`
   return (
-    <Heading as='h1'></Heading>
+    <Heading as='h1' marginY={5} fontSize="5xl" >{heading}</Heading>
     )
 }
 
